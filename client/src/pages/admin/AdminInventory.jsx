833cd7722feeaff73782dd0c8f322a6607050955
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-02 22:50:55 +0300

    add state for inventory entries and loading

--- client/src/pages/admin/AdminInventory.jsx
+++ client/src/pages/admin/AdminInventory.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardLayout from '../../components/layout/DashboardLayout';
 
 const AdminInventory = () => {
+  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  
   return <div>Admin Inventory</div>;
+
+  return (
+    <DashboardLayout title="Inventory Entries 📋">
+        <div>Admin Inventory</div>
+    </DashboardLayout>
+    );
+
 };
 
-return (
-  <DashboardLayout title="Inventory Entries 📋">
-    <div>Admin Inventory</div>
-  </DashboardLayout>
-);
+
 
 export default AdminInventory;
