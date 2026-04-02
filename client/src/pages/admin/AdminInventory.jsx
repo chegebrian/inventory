b15de178718671906c49302f5615fd0200dd99fe
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-02 22:47:17 +0300

    wrap AdminInventory with DashboardLayout

--- client/src/pages/admin/AdminInventory.jsx
+++ client/src/pages/admin/AdminInventory.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import DashboardLayout from '../../components/layout/DashboardLayout';
 
 const AdminInventory = () => {
   return <div>Admin Inventory</div>;
 };
+
+return (
+  <DashboardLayout title="Inventory Entries 📋">
+    <div>Admin Inventory</div>
+  </DashboardLayout>
+);
 
 export default AdminInventory;
