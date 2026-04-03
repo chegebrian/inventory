b6cc699118a4624e91e89da3ca57bf89ba703055
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 02:22:18 +0300

    add redux states

--- client/src/pages/admin/AdminClerks.jsx
+++ client/src/pages/admin/AdminClerks.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import DashboardLayout from '../../components/layout/DashboardLayout';
+import { useSelector } from 'react-redux';
 
 const AdminClerks = () => {
   const [clerks, setClerks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [inviteEmail, setInviteEmail] = useState('');
   const [deletingId, setDeletingId] = useState(null);
+  const { user } = useSelector(state => state.auth);
 
   return (
     <DashboardLayout title="Manage Clerks 👥">
