86e9ee5fcc3a1081f23d40deb39679cc9068a90d
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 02:20:18 +0300

    add AdminClerks component and dashboard layout

---
+++ client/src/pages/admin/AdminClerks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../components/layout/DashboardLayout';
+
+const AdminClerks = () => {
+  return (
+    <DashboardLayout title="Manage Clerks 👥">
+      <div>Clerks</div>
+    </DashboardLayout>
+  );
+};
+
+export default AdminClerks;
