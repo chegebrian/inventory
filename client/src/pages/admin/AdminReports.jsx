8e49c4adf31d855f9b7f0bc4e6d08fb964452893
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 00:54:17 +0300

    ceare AdminReports component and add dashboard layout

---
+++ client/src/pages/admin/AdminReports.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../components/layout/DashboardLayout';
+
+const AdminReports = () => {
+  return (
+    <DashboardLayout title="Reports 📈">
+      <div>Reports</div>
+    </DashboardLayout>
+  );
+};
+
+export default AdminReports;
