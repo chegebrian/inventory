c6e81a948a11e199a7806816cf365e65b168f570
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 03:09:46 +0300

    add state for requests and loading

--- client/src/pages/admin/AdminSupplyRequests.jsx
+++ client/src/pages/admin/AdminSupplyRequests.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DashboardLayout from '../../components/layout/DashboardLayout';
 
 const AdminSupplyRequests = () => {
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <DashboardLayout title="Supply Requests 🚚">
       <div>Supply Requests</div>
