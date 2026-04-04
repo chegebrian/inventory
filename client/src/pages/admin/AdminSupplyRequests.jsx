798b7abb1986e0ad0b4b1fa37a68c9c62b2ceab0
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 03:07:59 +0300

    create AdminSupplyRequests component and add dashboard layout

---
+++ client/src/pages/admin/AdminSupplyRequests.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../components/layout/DashboardLayout';
+
+const AdminSupplyRequests = () => {
+  return (
+    <DashboardLayout title="Supply Requests 🚚">
+      <div>Supply Requests</div>
+    </DashboardLayout>
+  );
+};
+
+export default AdminSupplyRequests;
