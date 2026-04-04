cfe183bec11e12ff8523a1f3b1c748a07366bec6
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-04 03:21:02 +0300

    add AdminProducts component and dashboard

---
+++ client/src/pages/admin/AdminProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLayout from '../../components/layout/DashboardLayout';
+
+const AdminProducts = () => {
+  return (
+    <DashboardLayout title="Products 📦">
+      <div>Admin Products</div>
+    </DashboardLayout>
+  );
+};
+
+export default AdminProducts;
