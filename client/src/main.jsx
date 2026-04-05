6421bfbf17c32aeb1285f5f41a75898105a71257
brianwaithaka <waithakabrian8@gmail.com>
2026-04-06 00:37:40 +0300

    add react toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 import App from "./App.jsx";
 
