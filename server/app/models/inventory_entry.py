b6f739fc023e631ac4f7647c3fe1c13c355b19a3
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-04-08 22:22:26 +0300

    add primary key and foreign key relationships

--- server/app/models/inventory_entry.py
+++ server/app/models/inventory_entry.py
@@ -3,3 +3,7 @@ from datetime import datetime
 
 class InventoryEntry(db.Model):
     __tablename__ = 'inventory_entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    store_product_id = db.Column(db.Integer, db.ForeignKey('store_products.id'), nullable=False)
+    clerk_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
