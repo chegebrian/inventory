bc7ddfda685da481388ec3ab800ef5883ec19fd6
brianwaithaka <waithakabrian8@gmail.com>
2026-04-19 13:36:42 +0300

    fix cors err

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -45,10 +45,7 @@ def create_app(config_name='default'):
         app,
         resources={
             r"/api/*": {
-                "origins": [
-                    "http://localhost:5173",
-                    "http://127.0.0.1:5173"
-                ]
+                "origins": "*"
             }
         },
         supports_credentials=True,
